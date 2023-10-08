d28bde88e5acd058eeaa75a4ab7f2c7d9ff8ae5e
ShaikhAlmas76 <shaikhalmas1484@gmail.com>
2023-10-08 23:59:27 +0530

    my-web

--- my-web/src/Functioncompo/12Usereducer.jsx
+++ my-web/src/Functioncompo/12Usereducer.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useReducer, useState } from 'react'
+
+const initialstate = 0
+
+const reducer = (state, action) => {
+    return state
+}
+
+
 
 const Usereducer = () => {
+    const [count, setCount] = useState(0)
+
+    const [state, dispatch] = useReducer(reducer, initialstate)
+
+
+    const increment = () => {
+        setCount(count + 1)
+    }
+
+    const decrement = () => {
+        setCount(count - 1)
+    }
+
     return (
         <>
             <h1> Usereducer </h1>
+
+            <h2> {count}</h2>
+            <button onClick={increment}>update</button>
+            <button onClick={decrement}>update</button>
         </>
     );
 }
