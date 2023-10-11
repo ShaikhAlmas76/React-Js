d76387ef4f034fade42713e5c53efd397355d53a
ShaikhAlmas76 <shaikhalmas1484@gmail.com>
2023-10-11 19:36:41 +0530

    my-web

--- my-web/src/Functioncompo/15Apipost.jsx
+++ my-web/src/Functioncompo/15Apipost.jsx
@@ -1,18 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Apipost = () => {
+    const [Name, setName] = useState("")
+    const [Email, setEmail] = useState("")
+    const [Password, setPassword] = useState("")
+
+    function savedata() {
+        // console.log(Name, Email, Password);
+
+        let data = { Name, Email, Password }
+        console.log(savedata);
+
+        fetch("http://localhost:4000/user", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data)
+        }).then((result) => {
+            console.log(result);
+            result.json().then((resp) => {
+                console.log(resp);
+            })
+        })
+    }
     return (
         <>
-            <h1> Api post </h1>
+            <h1 align='center'> Api post </h1>
 
 
-            <form >
+            <table width="25%" align='center' >
                 <tr>
-                    <td> Username <input type="text" placeholder='your-name' /></td>
-                    <td> email  <input type="email" placeholder='your-email' /></td>
-                    <td> password  <input type="password" placeholder='your-password' /></td>
+                    <td> Username:- </td>
+                    <td> <input type="text" value={Name} onChange={(e) => setName(e.target.value)} placeholder='your-name' /></td>
                 </tr>
-            </form>
+                <tr>
+                    <td> Email :-</td>
+                    <td> <input type="email" value={Email} onChange={(e) => setEmail(e.target.value)} placeholder='your-email' /></td>
+                </tr>
+                <tr>
+                    <td> Password :- </td>
+                    <td><input type="password" value={Password} onChange={(e) => setPassword(e.target.value)} placeholder='your-password' /></td>
+                </tr>
+
+                <tr>
+                    <td> <button onClick={savedata}> Submit</button> </td>
+                </tr>
+            </table >
 
 
             {/* <form>
