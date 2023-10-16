0ee7ce2ca0f7ca56d962dd97e9ac69ba593fa42a
ShaikhAlmas76 <shaikhalmas1484@gmail.com>
2023-10-16 23:52:56 +0530

    my-web

--- my-web/src/Functioncompo/20Crud.jsx
+++ my-web/src/Functioncompo/20Crud.jsx
@@ -15,18 +15,28 @@ const Crud = () => {
         }
 
     }
+    const handledel = (index) => {
+        console.log("handledel");
+        console.log(index);
+        alldata.splice(index, 1)
+        setAlldata([...alldata])
+    }
+    const handleedit = (i) => {
+        setName(alldata[i])
+    }
     return (
         <>
             <div className='input-fl'>
                 <input value={name} type="text" onChange={(e) => setName(e.target.value)} />
                 <button onClick={handleadd}> Add item </button>
+                <button onClick={handleedit}> Update </button>
             </div >
             {
-                alldata.map((val) =>
+                alldata.map((val, i) =>
                     <div className='alldata'>
                         <h1>{val}</h1>
-                        <button>edit</button>
-                        <button>delete</button>
+                        <button className="edit" onClick={() => handleedit(i)}>edit</button>
+                        <button className="del" onClick={() => handledel(i)}>delete</button>
                     </div>
 
 
