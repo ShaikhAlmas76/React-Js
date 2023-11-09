aeb60c0fda61cb680fc69dbbbb99eb0f12fed7a3
ShaikhAlmas76 <shaikhalmas1484@gmail.com>
2023-11-09 15:33:01 +0530

    react-login

--- React-login/src/Login.jsx
+++ React-login/src/Login.jsx
@@ -7,12 +7,13 @@ import {
     MDBIcon
 }
     from 'mdb-react-ui-kit';
-import { Navigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 function Login() {
     const [user, setUser] = useState()
     const [password, setPassword] = useState()
+    const Navigate = useNavigate()
 
 
     const login = () => {
@@ -21,43 +22,44 @@ function Login() {
             console.log(result);
             result.json().then((resp) => {
                 console.log(resp);
-                if (resp[0])
-                    sessionStorage.setItem('username', username)
-                sessionStorage.setItem('role', resp[0].role)
-                if (resp[0].role == 1) {
-                    Navigate("/admin")
+                if (resp[0]) {
+                    sessionStorage.setItem('name', user)
+                    sessionStorage.setItem('role', resp[0].role)
+                    if (resp[0].role == 1) {
+                        Navigate("/admin")
+                    } else {
+                        Navigate("/")
+                    }
                 } else {
-                    Navigate("/")
+                    alert("invalid user")
                 }
-                 else {
-        alert("invalid  user")
-    }
 
-})
+
+            })
         })
     }
 
-return (
-    <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
+    return (
+        <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
 
-        <MDBInput value={user} onChange={(e) => setUser(e.target.value)} wrapperClass='mb-4' label='user name' id='form1' type='text' />
-        <MDBInput value={password} onChange={(e) => setPassword(e.target.value)} wrapperClass='mb-4' label='Password' id='form2' type='password' />
+            <MDBInput value={user} onChange={(e) => setUser(e.target.value)} wrapperClass='mb-4' label='user name' id='form1' type='text' />
+            <MDBInput value={password} onChange={(e) => setPassword(e.target.value)} wrapperClass='mb-4' label='Password' id='form2' type='password' />
 
-        <div className="d-flex justify-content-between mx-3 mb-4">
-            <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember me' />
-            <a href="!#">Forgot password?</a>
-        </div>
+            <div className="d-flex justify-content-between mx-3 mb-4">
+                <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember me' />
+                <a href="!#">Forgot password?</a>
+            </div>
 
-        <MDBBtn className="mb-4" onClick={login}>Login</MDBBtn>
+            <MDBBtn className="mb-4" onClick={login}>Login</MDBBtn>
 
-        <div className="text-center">
-            <p>Not a member?
-                <Link to="/register">Register</Link>
-            </p>
-        </div>
+            <div className="text-center">
+                <p>Not a member?
+                    <Link to="/register">Register</Link>
+                </p>
+            </div>
 
-    </MDBContainer>
-);
+        </MDBContainer>
+    );
 }
 
 export default Login;
